707a48b5dae587fe56a9ad8b7b0dfbc2c448eaa0
unknown <afir4u@gmail.com>
2022-05-27 11:44:26 +0530

    added reports

--- src/test/java/org/data/BaseClass.java
+++ src/test/java/org/data/BaseClass.java
@@ -1,5 +1,70 @@
 package org.data;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import com.paulhammant.ngwebdriver.NgWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseClass {
+	public static WebDriver driver;
+	public static NgWebDriver ngdriver;
+	public static void launchBrowser(String Browser) {
+	if (Browser.equals("Chrome")) {
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		
+	}
+	else if (Browser.equals("Firefox")) {
+		WebDriverManager.firefoxdriver().setup();
+		driver= new FirefoxDriver();
+		
+	}
+	else {
+		WebDriverManager.firefoxdriver().setup();
+	}
+	}
+	public static void launchUrl(String Url) {
+		driver.get(Url);
+		String title = driver.getTitle();
+	System.out.println(title);
+		String currentUrl = driver.getCurrentUrl();
+	   System.out.println(currentUrl);
+	   driver.manage().window().maximize();
+	    
+	}
+     public static void quitBrowser() {
+     driver.close();
+	}
+	public static void clickButton(WebElement e) {
+		e.click();
+	}
+
+	public static void enterValue(WebElement e,String value) {
+		e.sendKeys(value);
+
+	}
+	public static void doubleclick(WebElement e) {
+      Actions a= new Actions(driver);
+      a.doubleClick(e).build().perform();
+	}
+	public static void movemouse(WebElement e) {
+		Actions a= new Actions(driver);
+		a.moveToElement(e);
+	}
+	
+	public static void selectOption(WebElement e , String value) {
+         Select s =new Select(e);
+         s.selectByVisibleText( value);
+	}	
+	
 
 }
+
+
+
--- src/test/java/org/data/NewJob.java
+++ src/test/java/org/data/NewJob.java
@@ -1,5 +1,102 @@
 package org.data;
 
-public class NewJob {
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.paulhammant.ngwebdriver.ByAngularOptions;
+import com.paulhammant.ngwebdriver.NgWebDriver;
+
+public class NewJob extends BaseClass{
+	@BeforeClass
+	private void startBrowser() {
+    launchBrowser("Chrome");
+	}
+	
+	//@AfterClass
+//	private void closeBrowser() {
+ //   quitBrowser();
+	//}
+ 
+	@BeforeMethod
+	private void startTime() {
+    Date d= new Date();
+    System.out.println(d);
+	}
+	
+	@AfterMethod
+	private void endTime() {
+		 Date d= new Date();
+		    System.out.println(d);
+	}
+	
+	
+	@Test
+	private void testCase1() {
+		JavascriptExecutor js =(JavascriptExecutor)driver;
+		ngdriver= new NgWebDriver(js);
+launchUrl("http://54.206.66.198:8082/#/login");
+ngdriver.waitForAngularRequestsToFinish();
+
+WebElement user = driver.findElement(By.id("username"));
+enterValue(user, "Shri");
+
+
+WebElement pass = driver.findElement(By.id("password"));
+enterValue(pass, "Admin@123");
+
+
+
+ WebElement log = driver.findElement(ByAngularOptions.xpath("//button[@type='submit']"));
+ clickButton(log);
+ driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+ 
+ 
+
+WebElement job = driver.findElement(ByAngularOptions.xpath("(//a[@tabindex='0'])[2]"));
+clickButton(job);
+driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+
+
+
+WebElement newjob = driver.findElement(ByAngularOptions.xpath("//button[@label='New']"));
+clickButton(newjob);
+
+driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+
+WebElement drop = driver.findElement(ByAngularOptions.xpath("(//div[@role='button'])[3]"));
+clickButton(drop);
+ 
+driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 
 }
